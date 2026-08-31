49fae8eed8cc01324420bc94033a8cd4f9faa2d7
Gozen0410 <98655920+Gozen0410@users.noreply.github.com>
2026-08-31 16:20:34 +0530

    Add early runtime diagnostics to isolate Borealis crash

--- switch/source/main.cpp
+++ switch/source/main.cpp
@@ -1,6 +1,13 @@
 #include <borealis.hpp>
 
+#include <cstdio>
 #include <cstdlib>
+
+static void log_stage(const char* stage)
+{
+    std::printf("[Saikou] %s\n", stage);
+    std::fflush(stdout);
+}
 
 class HomeActivity : public brls::Activity
 {
@@ -13,17 +20,29 @@ int main(int argc, char* argv[])
     (void)argc;
     (void)argv;
 
-    brls::Logger::setLogLevel(brls::LogLevel::INFO);
+    log_stage("entered main");
+    brls::Logger::setLogLevel(brls::LogLevel::DEBUG);
+    log_stage("logger configured");
 
     if (!brls::Application::init())
+    {
+        log_stage("Application::init FAILED");
         return EXIT_FAILURE;
+    }
+    log_stage("Application::init OK");
 
     brls::Application::createWindow("Saikou Switch");
+    log_stage("window created");
+
     brls::Application::setGlobalQuit(true);
+    log_stage("global quit configured");
+
     brls::Application::pushActivity(new HomeActivity());
+    log_stage("HomeActivity pushed");
 
     while (brls::Application::mainLoop())
         ;
 
+    log_stage("main loop ended");
     return EXIT_SUCCESS;
 }
