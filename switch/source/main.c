c9818adabf4393e509ac02eee131483a01bc9cc2
Gozen0410 <98655920+Gozen0410@users.noreply.github.com>
2026-08-31 15:24:32 +0530

    Use current libnx HID API for Plus exit

--- switch/source/main.c
+++ switch/source/main.c
@@ -1,4 +1,5 @@
 #include <switch.h>
+#include <switch/services/hid.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
@@ -10,11 +11,25 @@ int main(int argc, char* argv[]) {
     printf("Saikou Switch\n\n");
     printf("Native Switch port bootstrap\n");
     printf("\n");
-    printf("Milestone 0: libnx + input loop\n");
+    printf("Milestone 0: libnx + controller input\n");
     printf("\n");
     printf("Press + to exit.\n");
 
     while (appletMainLoop()) {
+        hidScanInput();
+
+        HidNpadFullKeyState state;
+        size_t count = hidGetNpadStatesFullKey(HidNpadIdType_No1, &state, 1);
+
+        if (count > 0 && (state.buttons & HidNpadButton_Plus))
+            break;
+
+        HidNpadHandheldState handheld;
+        count = hidGetNpadStatesHandheld(HidNpadIdType_Handheld, &handheld, 1);
+
+        if (count > 0 && (handheld.buttons & HidNpadButton_Plus))
+            break;
+
         consoleUpdate(NULL);
     }
 
