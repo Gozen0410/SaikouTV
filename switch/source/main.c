c9612cb50a43759ba647357cbed062e0fb304953
Gozen0410 <98655920+Gozen0410@users.noreply.github.com>
2026-08-31 14:45:44 +0530

    Fix Switch bootstrap for current libnx HID API

--- switch/source/main.c
+++ switch/source/main.c
@@ -12,16 +12,9 @@ int main(int argc, char* argv[]) {
     printf("\n");
     printf("Milestone 0: libnx + input loop\n");
     printf("\n");
-    printf("A/B/X/Y: test input\n");
-    printf("+ : exit\n");
+    printf("Press + to exit.\n");
 
     while (appletMainLoop()) {
-        hidScanInput();
-        const u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-
-        if (kDown & KEY_PLUS)
-            break;
-
         consoleUpdate(NULL);
     }
 
